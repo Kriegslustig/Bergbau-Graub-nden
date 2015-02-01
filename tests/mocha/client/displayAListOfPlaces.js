1b73efa26e8778cf9755b407acc00d55494e4a16
Kriegslustig <github@ls7.ch>
2015-02-01 16:13:37 +0100

    Remove hack is was breaking the tests

--- tests/mocha/client/displayAListOfPlaces.js
+++ tests/mocha/client/displayAListOfPlaces.js
@@ -1,33 +1,31 @@
 if (!(typeof MochaWeb === 'undefined')){
   MochaWeb.testOnly(function () {
-    setTimeout(function (){
-      describe('The Route "/"', function () {
-        it('Should have the title "Map"', function () {
-          var title = document.title
-          console.log(title)
-          chai.assert.include(title, 'Map')
-        })
+    Meteor.flush()
+    describe('The Route "/"', function () {
+      it('Should have the title "Map"', function () {
+        var title = document.title
+        chai.assert.include(title, 'Map')
       })
-      describe('A list of places', function () {
-        it('Should have a main section with the class map', function () {
-          var map = $('main.map')
-          chai.assert.ok(map.length > 0)
-        })
-        it('Should contain map__places', function () {
-          var placesList = $('main.map ul.map__places')
-          chai.assert.ok(placesList.length > 0)
-        })
-        it('should at least contain one place', function () {
-          var places = $('ul.map__places li.place')
-          chai.assert.ok(places.length > 0)
-        })
+    })
+    describe('A list of places', function () {
+      it('Should have a main section with the class map', function () {
+        var map = $('main.map')
+        chai.assert.ok(map.length > 0)
       })
-      describe('A place', function () {
-        it('Should include a location', function () {
-          var title = $('li.place .place__location').text()
-          chai.assert.ok(title.length > 0)
-        })
+      it('Should contain map__places', function () {
+        var placesList = $('main.map ul.map__places')
+        chai.assert.ok(placesList.length > 0)
       })
-    }, 1000)
+      it('should at least contain one place', function () {
+        var places = $('ul.map__places li.place')
+        chai.assert.ok(places.length > 0)
+      })
+    })
+    describe('A place', function () {
+      it('Should include a location', function () {
+        var title = $('li.place .place__location').text()
+        chai.assert.ok(title.length > 0)
+      })
+    })
   })
 }
