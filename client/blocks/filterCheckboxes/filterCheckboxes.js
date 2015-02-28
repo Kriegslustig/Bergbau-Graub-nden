e5a6b27cf9090d56b36df67a156a60ca4d4cc99b
Kriegslustig <github@ls7.ch>
2015-03-01 00:19:14 +0100

    Fix countless syntax errors

--- client/blocks/filterCheckboxes/filterCheckboxes.js
+++ client/blocks/filterCheckboxes/filterCheckboxes.js
@@ -13,27 +13,17 @@ FilterCheckboxes = {
       self.items.forEach(function (item) {
         self.list.appendChild(Blaze.renderWithData(Template.filterCheckboxesCheckbox, item))
       })
-      self.allElements = self.list.querySelectorAll(self.wrapperClassName + '__item'
+      self.allElements = self.list.querySelectorAll(self.wrapperClassName + '__item')
       self.setEvents()
     }
   }
 , setEvents: function () {
     var self = this
-    _.each(self.allElements), function (element) {
+    _.each(self.allElements, function (element) {
       element.addEventListener('change', function () {
         self.updateTrueList()
       })
     })
-  }
-, updateTrueList: () {
-    var self = this
-    self._trueList = []
-    _.each(self.allElements function (element) {
-      if(element.checked) {
-        self._trueList.push(element.name)
-      }
-    })
-    _trueListDep.changed()
   }
 , _trueList: []
 , _trueListDep: new Tracker.Dependency
@@ -41,5 +31,15 @@ FilterCheckboxes = {
     var self = this
     _trueListDep.depend()
     return self._trueList
+  }
+, updateTrueList: function () {
+    var self = this
+    self._trueList = []
+    _.each(self.allElements, function (element) {
+      if(element.checked) {
+        self._trueList.push(element.name)
+      }
+    })
+    _trueListDep.changed()
   }
 }
