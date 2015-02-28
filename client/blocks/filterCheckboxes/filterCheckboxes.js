7a53dfc16e4e047e0f9cf9cd0e65074aac1fc08f
Kriegslustig <github@ls7.ch>
2015-03-01 00:02:52 +0100

    Add a basic version of filterCheckboxes

---
+++ client/blocks/filterCheckboxes/filterCheckboxes.js
@@ -0,0 +1,45 @@
+FilterCheckboxes = {
+  title: 'Checkboxes'
+, name: 'checkboxes'
+, items: [{
+    label: 'Example'
+  , name: 'example'
+  }]
+, wrapperClassName: 'filterCheckboxes'
+, init: function () {
+    var self = this
+    self.list = document.querySelector('.' + self.wrapperClassName + ' ' + self.wrapperClassName + '__list')
+    if(self.list) {
+      self.items.forEach(function (item) {
+        self.list.appendChild(Blaze.renderWithData(Template.filterCheckboxesCheckbox, item))
+      })
+      self.allElements = self.list.querySelectorAll(self.wrapperClassName + '__item'
+      self.setEvents()
+    }
+  }
+, setEvents: function () {
+    var self = this
+    _.each(self.allElements), function (element) {
+      element.addEventListener('change', function () {
+        self.updateTrueList()
+      })
+    })
+  }
+, updateTrueList: () {
+    var self = this
+    self._trueList = []
+    _.each(self.allElements function (element) {
+      if(element.checked) {
+        self._trueList.push(element.name)
+      }
+    })
+    _trueListDep.changed()
+  }
+, _trueList: []
+, _trueListDep: new Tracker.Dependency
+, getTrueList: function () {
+    var self = this
+    _trueListDep.depend()
+    return self._trueList
+  }
+}
