7dae8acb40df5f4298acfd1d5e640ddd292b6b9c
Kriegslustig <github@ls7.ch>
2015-03-01 17:48:14 +0100

    Add pageUsers

--- client/routes.js
+++ client/routes.js
@@ -8,3 +8,18 @@ Router.route('/', {
     self.render('pageMap')
   }
 })
+
+Router.route('/users', {
+  name: 'users'
+, waitOn: function () {
+    if(Meteor.user()) {
+      return Meteor.subscribe('usersList')
+    } else {
+      return true
+    }
+  }
+, action: function () {
+    var self = this
+    self.render('pageUsers')
+  }
+})
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,11 @@
 Meteor.publish('placesList', function () {
   return Places.find({}, {fields: {'images': false}})
 })
+
+Meteor.publish('usersList', function () {
+  if(Meteor.user()) {
+    return Meteor.users.find({}, {fields: {'username': true}})
+  } else {
+    return false;
+  }
+})
