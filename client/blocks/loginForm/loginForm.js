b39da2d5bae94d7af0a0b641cf0f9315225452df
Kriegslustig <github@ls7.ch>
2015-03-01 18:14:43 +0100

    Log login errors

--- client/blocks/loginForm/loginForm.js
+++ client/blocks/loginForm/loginForm.js
@@ -2,7 +2,11 @@ Template.loginForm.events({
   'submit .loginForm': function (e) {
     var username = e.target[0].value
   , password = e.target[1].value
-    Meteor.loginWithPassword(username, password)
+    Meteor.loginWithPassword(username, password, function (err) {
+      if(err) {
+        console.log(err)
+      }
+    })
 
     return false
   }
