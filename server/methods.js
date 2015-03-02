14ec61364f2dd73dc680ec7fb76d91cfe7841d2f
Kriegslustig <github@ls7.ch>
2015-03-02 23:12:06 +0100

    Add proper Owner fields

--- server/methods.js
+++ server/methods.js
@@ -1,8 +1,16 @@
 Meteor.methods({
   newPlace: function (data) {
-    if(!Meter.user()) return false
+    if(!Meteor.user()) return false
+    console.log(data)
     check(data, Schema.place)
-    Places.insert({
+    var owners = _.map(data.owners, function (owner) {
+      return {
+        name: owner.ownerName
+      , start: owner.start
+      , stop: owner.stop
+      }
+    })
+  , newPlace = {
       location: {
         name: data.locationName
       , coordinates: {
@@ -15,11 +23,13 @@ Meteor.methods({
     , finds: data.finds
     , images: data.images
     , references: data.references
-    , owners: data.owners
+    , owners: owners
     , usage: {
         start: data.usageStart
       , stop: data.usageStop
       }
-    })
+    }
+    console.log(newPlace)
+    Places.insert(newPlace)
   }
 })
