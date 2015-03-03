f83fc63e78d423d05ccdb47567c940befb939644
Kriegslustig <github@ls7.ch>
2015-03-03 19:27:28 +0100

    Make the timeRange clickable

--- client/blocks/timeRange/timeRange.js
+++ client/blocks/timeRange/timeRange.js
@@ -11,16 +11,17 @@ timeRange = {
     var self = this
     self.element = document.querySelector('.' + self.config.elemClass)
     self.bar = document.querySelector('.' + self.config.barElemClass)
+    if(self.bar && self.element) self.setListeners()
   }
 , stepUp: function () {
     var self = this
-    self.setPosition(self.position + self.config.step)
+    self.setAbstractPosition(self.position + self.config.step)
   }
 , stepDown: function () {
     var self = this
-    self.setPosition(self.position - self.config.step)
+    self.setAbstractPosition(self.position - self.config.step)
   }
-, setPosition: function (newPosition) {
+, setAbstractPosition: function (newPosition) {
     var self = this
     newPosition = newPosition > self.max ? self.max : newPosition
     newPosition = newPosition < 0 ? 0 : newPosition
@@ -31,11 +32,24 @@ timeRange = {
     })
     self.element.dispatchEvent(self.events.change)
   }
+, setRealPosition: function (newPosition) {
+    var self = this
+  , abstractPos = 0
+    abstractPos = Math.round( newPosition / ( self.element.clientWidth / self.max ) / self.config.step ) * self.config.step
+    console.log(newPosition, abstractPos)
+    self.setAbstractPosition(abstractPos)
+  }
 , updateElementPosition: function () {
     var self = this
     if(self.element) {
       self.bar.style.left = Math.round(self.element.clientWidth / self.max * self.position) + 'px'
     }
+  }
+, setListeners: function () {
+    var self = this
+    self.element.addEventListener('click', function (e) {
+      self.setRealPosition(e.x)
+    })
   }
 }
 
